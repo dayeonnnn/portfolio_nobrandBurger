81d5c00d8cbe66525bc79db0c398a1df10d1053b
dayeon <tjdekdus@naver.com>
2023-08-29 15:15:16 +0900

    top

--- script/common.js
+++ script/common.js
@@ -3,7 +3,20 @@ const menu_sub = document.querySelector('.menu')
 const news_sub = document.querySelector('.news')
 const m_nav = document.querySelector('.m_nav')
 const m_nav_open = document.querySelector('.m_nav_open')
-console.log(menu)
+const topBtn = document.querySelector('#topBtn')
+topBtn.style.display = 'none'
+window.addEventListener('scroll',function(){
+    if(this.window.pageYOffset > 300){
+        topBtn.style.display='block'
+    }else{
+        topBtn.style.display='none'
+    }
+})
+topBtn.addEventListener('click',function(){
+    //window.scrollTo(0, 100) //or
+    window.scrollTo({top:0, behavior:'smooth'})
+})
+
 menu[1].addEventListener('mouseover',function(){
     menu_sub.style.display = 'block'
 })
